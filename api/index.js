21e99f663e3d016964c4fa633618a7d5210eb991
carollyb <carolly.barbosa@gmail.com>
2022-05-02 22:04:50 -0300

    :sparkles: creates study repo

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require("apollo-server");
+const userSchema = require("./user/schema/user.graphql")
+
+const users = [
+    {
+        nome: "Ana",
+        ativo: true
+    },
+    {
+        nome: "Marcia",
+        ativo: false
+    }
+]
+
+const typeDefs = [userSchema]
+const resolvers = {}
+
+const server = new ApolloServer({ typeDefs, resolvers })
