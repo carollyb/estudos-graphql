567c2a34bed001aa29d49ba0f152a3be3e916e10
carollyb <carolly.barbosa@gmail.com>
2022-05-04 18:19:28 -0300

    :sparkles: joining different endpoints

--- api/user/resolvers/userResolver.js
+++ api/user/resolvers/userResolver.js
@@ -1,18 +1,7 @@
-const arrayUsers = [
-    {
-        nome: "Carolly",
-        ativo: true
-    },
-    {
-        nome: "Nayara",
-        ativo: false
-    }
-]
-
 const userResolvers = {
     Query: {
-        users: () => arrayUsers,
-        primeiroUser: () => arrayUsers[0]
+        users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
+        user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserById(id)      
     }
 }
 
